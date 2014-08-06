12e708f45d0bdaa7a0cdedc2111eb1a755c5698f
金涛 <jintao.ljt@alibaba-inc.com>
2014-08-06 16:50:42 +0800

    first commit

---
+++ Classes/CoverLayer.cpp
@@ -0,0 +1,9 @@
+//
+//  CoverLayer.cpp
+//  CrazyWord
+//
+//  Created by jintao on 14-8-6.
+//
+//
+
+#include "CoverLayer.h"
---
+++ Classes/CoverLayer.h
@@ -0,0 +1,21 @@
+//
+//  CoverLayer.h
+//  CrazyWord
+//
+//  Created by jintao on 14-8-6.
+//
+//
+
+#ifndef __CrazyWord__CoverLayer__
+#define __CrazyWord__CoverLayer__
+
+#include <iostream>
+#include "cocos2d.h"
+
+class CoverLayer : public Layer
+{
+    
+
+};
+
+#endif /* defined(__CrazyWord__CoverLayer__) */
