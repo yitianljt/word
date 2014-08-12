2b222101e39e32a0a07352c2512755479904a028
金涛 <jintao.ljt@alibaba-inc.com>
2014-08-12 23:39:45 +0800

    no message

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -10,11 +10,32 @@ LOCAL_MODULE := cocos2dcpp_shared
 
 LOCAL_MODULE_FILENAME := libcocos2dcpp
 
-LOCAL_SRC_FILES := hellocpp/main.cpp \
-                   ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp
 
+
+
+define walk  
+$(wildcard $(1)) $(foreach e, $(wildcard $(1)/*), $(call walk, $(e)))  
+endef 
+
+ALLFILES = $(call walk, $(LOCAL_PATH)/../../Classes)
+
+
+
+FILE_LIST := hellocpp/main.cpp  
+FILE_LIST += $(filter %.cpp, $(ALLFILES))  
+LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%)  
+
+
+#LOCAL_SRC_FILES := hellocpp/main.cpp \
+#                   ../../Classes/AppDelegate.cpp \
+#                   ../../Classes/HelloWorldScene.cpp
+#
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../cocos2d/extensions/
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../Classes/Util/
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../Classes/common/
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../Classes/model/
+
 
 LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocosdenshion_static
@@ -22,9 +43,9 @@ LOCAL_WHOLE_STATIC_LIBRARIES += cocosdenshion_static
 # LOCAL_WHOLE_STATIC_LIBRARIES += box2d_static
 # LOCAL_WHOLE_STATIC_LIBRARIES += cocosbuilder_static
 # LOCAL_WHOLE_STATIC_LIBRARIES += spine_static
-# LOCAL_WHOLE_STATIC_LIBRARIES += cocostudio_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocostudio_static
 # LOCAL_WHOLE_STATIC_LIBRARIES += cocos_network_static
-# LOCAL_WHOLE_STATIC_LIBRARIES += cocos_extension_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocos_extension_static
 
 
 include $(BUILD_SHARED_LIBRARY)
@@ -35,6 +56,6 @@ $(call import-module,audio/android)
 # $(call import-module,Box2D)
 # $(call import-module,editor-support/cocosbuilder)
 # $(call import-module,editor-support/spine)
-# $(call import-module,editor-support/cocostudio)
+$(call import-module,editor-support/cocostudio)
 # $(call import-module,network)
-# $(call import-module,extensions)
+$(call import-module,extensions)
